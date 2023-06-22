8e1c9737751921452ca05d8800ee0d5d574c86b4
Olayinka <olayinka4grace@gmail.com>
2023-06-22 19:12:19 +0100

    chat screen added

--- client/src/navigations/TopTabNav.js
+++ client/src/navigations/TopTabNav.js
@@ -1,27 +1,25 @@
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import StatusScreen from '../screens/StatusScreen';
-import HomeScreen from '../screens/HomeScreen';
-import CallScreen from '../screens/CallScreen';
-import AdvertScreen from '../screens/AdvertScreen';
-import { NativeStack } from './NativeStack';
-import { MaterialIcons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import StatusScreen from "../screens/StatusScreen";
+import CallScreen from "../screens/CallScreen";
+import AdvertScreen from "../screens/AdvertScreen";
+import { NativeStack } from "./NativeStack";
+import { MaterialIcons } from "@expo/vector-icons";
 
-const Tab = createMaterialTopTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export function TopTabNav() {
   return (
-    <Tab.Navigator 
-    screenOptions={{
-      tabBarActiveTintColor: 'white',
-      tabBarInactiveTintColor: 'lightgrey',
-      tabBarStyle: {backgroundColor: '#016064'},
-
-    }}
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "lightgrey",
+        tabBarStyle: { backgroundColor: "#016064" },
+      }}
     >
-      <Tab.Screen name="Chat" component={NativeStack} />
-      <Tab.Screen name="Status" component={StatusScreen} />
-      <Tab.Screen name="Call" component={CallScreen} />
-      <Tab.Screen name="Advert" component={AdvertScreen} />
+      <Tab.Screen name='Chat' component={NativeStack} />
+      <Tab.Screen name='Status' component={StatusScreen} />
+      <Tab.Screen name='Call' component={CallScreen} />
+      <Tab.Screen name='Advert' component={AdvertScreen} />
     </Tab.Navigator>
   );
 }
--- client/src/screens/ChatScreen.js
+++ client/src/screens/ChatScreen.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import {ImageBackground, StyleSheet, FlatList } from "react-native";
+import background from "../../assets/images/BG.png";
+import Message from "../components/Message";
+import messages from '../../assets/data/messages.json'
 
 const ChatScreen = () => {
   return (
-   <View>
-    <Text>ChatScreen</Text>
-   </View>
-  )
-}
+    <ImageBackground source={background} style={styles.bg}>
+    <FlatList
+      data={messages}
+      renderItem={({ item }) => <Message message={item} />}
+      style={styles.list}
+      inverted
+    />
+  </ImageBackground>
+  );
+};
 
-export default ChatScreen
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1
+  },
+  list: {
+    padding: 10,
+  }
+})
+
+
+export default ChatScreen;
