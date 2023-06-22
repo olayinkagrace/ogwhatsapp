3ec3011174f27c71de780d0656c53d2bf6c1a516
Olayinka <olayinka4grace@gmail.com>
2023-06-22 17:52:58 +0100

    chat list completed

--- client/src/components/ChatList.js
+++ client/src/components/ChatList.js
@@ -1,45 +1,53 @@
 import React from "react";
-import { Image, Text, View, StyleSheet } from "react-native";
+import { Image, Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
-const ChatList = ({ chat }) => {
-  console.log(chat.user);
+const ChatList = ({ chat, navigation }) => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri: chat.user.image,
-        }}
-        style={styles.img}
-      />
-      <View>
-        <Text style={styles.bold}>{chat.user.name}</Text>
-        <Text style={styles.light} numberOfLines={1}>
-          {chat.lastMessage.text}
-        </Text>
-      </View>
-      <View>
-        <Text style={styles.time}>{chat.lastMessage.createdAt}</Text>
-        <View style={styles.messageIcon}>
-          <MaterialCommunityIcons
-            name='checkbox-blank-circle'
-            size={22}
-            color='green'
-          />
-          <Text style={styles.message}>5</Text>
+    <TouchableOpacity onPress={() => navigation.navigate("ChatScreen")}>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri: chat.user.image,
+          }}
+          style={styles.img}
+        />
+        <View style={styles.content}>
+          <Text style={styles.bold}>{chat.user.name}</Text>
+          <Text style={styles.light} numberOfLines={1}>
+            {chat.lastMessage.text}
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.time}>{dayjs(chat.lastMessage.createdAt).fromNow()}</Text>
+          <View style={styles.messageIcon}>
+            <MaterialCommunityIcons
+              name='checkbox-blank-circle'
+              size={22}
+              color='green'
+            />
+            <Text style={styles.message}>5</Text>
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    margin: 10,
+    flexDirection: 'row',
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 70,
     // borderBottomWidth: StyleSheet.hairlineWidth,
     // borderBottomColor: 'lightgrey'
+  },
+  content: {
+    flex: 1
   },
   bold: {
     fontWeight: "600",
@@ -67,9 +75,10 @@ const styles = StyleSheet.create({
     marginLeft: -15,
   },
   img: {
-    width: 40,
-    height: 40,
-    borderRadius: 50,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
   },
 });
 
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { Text, View, Button, ScrollView } from "react-native";
+import { Text, View, Button, ScrollView, FlatList, StyleSheet } from "react-native";
 import ChatList from "../components/ChatList";
+import chats from '../../assets/data/chats.json'
 
-const chat = {
-  id: "1",
-  user: {
-    image:
-      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-    name: "Olayinka",
-  },
-  lastMessage: {
-    text: "how are you doing guy?",
-    createdAt: "07:37",
-  },
-};
+
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <ScrollView>
-      <ChatList chat={chat} />
-      <Button
-        title={"ChatScreen"}
-        onPress={() => navigation.navigate("ChatScreen")}
+    <View style={styles.container}>
+      <FlatList
+        data={chats}
+        renderItem={({item}) => <ChatList chat={item} navigation={navigation} />}
+        keyExtractor={item => item.id}
       />
-    </ScrollView>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    paddingVertical: 50,
+    alignItems: 'stretch'
+  },
+});
 
 export default HomeScreen;
